9acfde82bd0c7aa5c833e6b688c102bdc45acb13
Thais Camilo <dev+narwen@hashrocket.com>
2013-05-28 16:21:22 -0400

    ColorViewer: add multiple controllers

---
+++ ColorViewer/app/controllers/color_detail_controller.rb
@@ -0,0 +1,14 @@
+class ColorDetailController < UIViewController
+  def initWithColor(color)
+    self.initWithNibName(nil, bundle: nil)
+    @color = color
+    self
+  end
+
+  def viewDidLoad
+    super
+
+    self.view.backgroundColor = @color
+    self.title = 'Detail'
+  end
+end
