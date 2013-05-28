24e8d47a52074341b820c286effd8f50892ae814
Thais Camilo <dev+narwen@hashrocket.com>
2013-05-28 11:29:04 -0400

    ColorViewer: using controllers

---
+++ ColorViewer/app/controllers/colors_controller.rb
@@ -0,0 +1,16 @@
+class ColorsController < UIViewController
+  def viewDidLoad
+    super
+
+    self.view.backgroundColor = UIColor.whiteColor
+
+    @label = UILabel.alloc.initWithFrame(CGRectZero)
+    @label.text = "Colors"
+    @label.sizeToFit
+    @label.center = [self.view.frame.size.width / 2,
+                     self.view.frame.size.height / 2]
+    @label.autoresizingMask =
+      UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin
+    self.view.addSubview(@label)
+  end
+end
---
+++ ColorViewer/build/iPhoneSimulator-6.1-Development/objs/init.mm
@@ -0,0 +1,43 @@
+extern "C" {
+    void ruby_sysinit(int *, char ***);
+    void ruby_init(void);
+    void ruby_init_loadpath(void);
+    void ruby_script(const char *);
+    void ruby_set_argv(int, char **);
+    void rb_vm_init_compiler(void);
+    void rb_vm_init_jit(void);
+    void rb_vm_aot_feature_provide(const char *, void *);
+    void *rb_vm_top_self(void);
+    void rb_rb2oc_exc_handler(void);
+    void rb_exit(int);
+void MREP_1D8833E29E3B46FAA8E1BACD96F88E34(void *, void *);
+void MREP_6B52F3F11BD74F5D8F5E0B3ADF35984F(void *, void *);
+}
+
+extern "C"
+void
+RubyMotionInit(int argc, char **argv)
+{
+    static bool initialized = false;
+    if (!initialized) {
+	ruby_init();
+	ruby_init_loadpath();
+        if (argc > 0) {
+	    const char *progname = argv[0];
+	    ruby_script(progname);
+	}
+#if !__LP64__
+	try {
+#endif
+	    void *self = rb_vm_top_self();
+MREP_1D8833E29E3B46FAA8E1BACD96F88E34(self, 0);
+MREP_6B52F3F11BD74F5D8F5E0B3ADF35984F(self, 0);
+#if !__LP64__
+	}
+	catch (...) {
+	    rb_rb2oc_exc_handler();
+	}
+#endif
+	initialized = true;
+    }
+}
