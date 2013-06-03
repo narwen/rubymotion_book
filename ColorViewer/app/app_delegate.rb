9acd62be249788ecdc5c8584e35ba662ec3fe4f5
Thais Camilo <dev+narwen@hashrocket.com>
2013-06-03 10:37:54 -0400

    ColorView: add UITabBarController

--- ColorViewer/app/app_delegate.rb
+++ ColorViewer/app/app_delegate.rb
@@ -6,7 +6,15 @@ class AppDelegate
     controller =  ColorsController.alloc.initWithNibName(nil, bundle: nil)
     nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
 
-    @window.rootViewController =  nav_controller
+    top_controller = ColorDetailController.alloc.initWithColor(UIColor.purpleColor)
+    top_controller.title = 'Top Color'
+
+    top_nav_controller = UINavigationController.alloc.initWithRootViewController(top_controller)
+
+    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
+    tab_controller.viewControllers = [nav_controller, top_nav_controller]
+
+    @window.rootViewController =  tab_controller
     true
   end
 end
--- ColorViewer/app/controllers/colors_controller.rb
+++ ColorViewer/app/controllers/colors_controller.rb
@@ -1,4 +1,10 @@
 class ColorsController < UIViewController
+  def initWithNibName(name, bundle: bundle)
+    super
+    self.tabBarItem = UITabBarItem.alloc.initWithTitle('Colors', image: nil, tag: 1)
+    self
+  end
+
   def viewDidLoad
     super
 
