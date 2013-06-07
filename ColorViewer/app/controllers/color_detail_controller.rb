039b6fcc4b59ac6868fd55ed5cdf1292496e3e35
Thais Camilo <dev+narwen@hashrocket.com>
2013-06-07 14:49:38 -0400

    ColorViewer: add UIBarButtonItem

--- ColorViewer/app/controllers/color_detail_controller.rb
+++ ColorViewer/app/controllers/color_detail_controller.rb
@@ -10,5 +10,21 @@ class ColorDetailController < UIViewController
 
     self.view.backgroundColor = @color
     self.title = 'Detail'
+
+    right_button = UIBarButtonItem.alloc.initWithTitle(
+      'Change',
+      style: UIBarButtonItemStylePlain,
+      target: self,
+      action: 'change_color')
+    self.navigationItem.rightBarButtonItem = right_button
+  end
+
+  def change_color
+    controller = ChangeColorController.alloc.initWithNibName(nil, bundle: nil)
+    controller.color_detail_controller = self
+    self.presentViewController(
+      UINavigationController.alloc.initWithRootViewController(controller),
+      animated: true,
+      completion: lambda {})
   end
 end
